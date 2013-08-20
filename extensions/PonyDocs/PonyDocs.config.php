43ba5bb45e83e921d69d8b32bf6a8fe08aef654e
Erik <erik@erikkristensen.com>
2013-08-20 09:16:29 -0400

    reorder some config items, allow PonyDocs constants to be overriden in LocalSettings.php

--- extensions/PonyDocs/PonyDocs.config.php
+++ extensions/PonyDocs/PonyDocs.config.php
@@ -3,69 +3,88 @@
 /**
  * PonyDocs Extension Configuration
  * 
+ */
+
+/**
+ * Checks if constant is already defined
+ */
+function pdefine($constant, $value) {
+	if (!defined($constant))
+		define($constant, $value);
+}
+
+/**
+ * Settings a user can alter, but do so in LocalSettings.php, NOT HERE
+ */
+pdefine('PONYDOCS_PRODUCT_LOGO_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/extensions/PonyDocs/images/pony.png');
+pdefine('PONYDOCS_PDF_COPYRIGHT_MESSAGE', 'Copyright Your Company, Inc. All Rights Reserved');
+pdefine('PONYDOCS_PDF_TITLE_IMAGE_PATH', '/extensions/PonyDocs/images/pony.png');
+pdefine('PONYDOCS_ENABLE_BRANCHINHERIT_EMAIL', true);
+
+
+/**
  * You really should not need to edit anything here!
  * 
- * Seriously!
- * 
- * 
+ * Seriously! You have been warned.
  */
 
 // Define your user groups here
-define('PONYDOCS_EMPLOYEE_GROUP', 'employees');
-define('PONYDOCS_BASE_AUTHOR_GROUP', 'docteam');
-define('PONYDOCS_BASE_PREVIEW_GROUP', 'preview');
-define('PONYDOCS_CRAWLER_AGENT_REGEX', '/gsa-crawler/');
+pdefine('PONYDOCS_EMPLOYEE_GROUP', 'employees');
+pdefine('PONYDOCS_BASE_AUTHOR_GROUP', 'docteam');
+pdefine('PONYDOCS_BASE_PREVIEW_GROUP', 'preview');
+pdefine('PONYDOCS_CRAWLER_AGENT_REGEX', '/gsa-crawler/');
 
 // Define Namespace information
-define('PONYDOCS_DOCUMENTATION_NAMESPACE_NAME', 'Documentation');
-define('PONYDOCS_DOCUMENTATION_NAMESPACE_ID', 100);
+pdefine('PONYDOCS_DOCUMENTATION_NAMESPACE_NAME', 'Documentation');
+pdefine('PONYDOCS_DOCUMENTATION_NAMESPACE_ID', 100);
 
-define('PONYDOCS_LANGUAGE_DEFAULT', 'en'); 
+// Default Language for the Site.
+pdefine('PONYDOCS_LANGUAGE_DEFAULT', 'en'); 
 // Automatically translate WikiDocs UI into documentations language format.
 // TODO: Fix, Buggy -- This is buggy, not sure it should say in.
-define('PONYDOCS_LANGUAGE_AUTOUI', false);
+pdefine('PONYDOCS_LANGUAGE_AUTOUI', false);
 // Always include language in URL, to include the default language.
-define('PONYDOCS_LANGUAGE_ALWAYS', false);
+pdefine('PONYDOCS_LANGUAGE_ALWAYS', false);
 
-define('PONYDOCS_CACHE_ENABLED', true);
-define('PONYDOCS_CACHE_DEBUG', false);
-define('PONYDOCS_REDIRECT_DEBUG', false);
-define('PONYDOCS_SESSION_DEBUG', false);
-define('PONYDOCS_AUTOCREATE_DEBUG', false);
-define('PONYDOCS_CASE_INSENSITIVE_DEBUG', false);
-define('PONYDOCS_DOCLINKS_DEBUG', false);
+pdefine('PONYDOCS_CACHE_ENABLED', true);
+pdefine('PONYDOCS_CACHE_DEBUG', false);
+pdefine('PONYDOCS_REDIRECT_DEBUG', false);
+pdefine('PONYDOCS_SESSION_DEBUG', false);
+pdefine('PONYDOCS_AUTOCREATE_DEBUG', false);
+pdefine('PONYDOCS_CASE_INSENSITIVE_DEBUG', false);
+pdefine('PONYDOCS_DOCLINKS_DEBUG', false);
 
-define('PONYDOCS_DOCUMENTATION_PREFIX', PONYDOCS_DOCUMENTATION_NAMESPACE_NAME . ':' );
+pdefine('PONYDOCS_DOCUMENTATION_PREFIX', PONYDOCS_DOCUMENTATION_NAMESPACE_NAME . ':' );
 
-define('PONYDOCS_DOCUMENTATION_PRODUCTS_TITLE', PONYDOCS_DOCUMENTATION_PREFIX . 'Products' );
-define('PONYDOCS_PRODUCT_LEGALCHARS', 'A-Za-z0-9_,.-' );
-define('PONYDOCS_PRODUCT_REGEX', '/([' . PONYDOCS_PRODUCT_LEGALCHARS . ']+)/' );
-define('PONYDOCS_PRODUCT_STATIC_PREFIX', '.');
+pdefine('PONYDOCS_DOCUMENTATION_PRODUCTS_TITLE', PONYDOCS_DOCUMENTATION_PREFIX . 'Products' );
+pdefine('PONYDOCS_PRODUCT_LEGALCHARS', 'A-Za-z0-9_,.-' );
+pdefine('PONYDOCS_PRODUCT_REGEX', '/([' . PONYDOCS_PRODUCT_LEGALCHARS . ']+)/' );
+pdefine('PONYDOCS_PRODUCT_STATIC_PREFIX', '.');
 
-define('PONYDOCS_PRODUCTVERSION_SUFFIX', ':Versions' );
-define('PONYDOCS_PRODUCTVERSION_LEGALCHARS', 'A-Za-z0-9_,.-' );
-define('PONYDOCS_PRODUCTVERSION_REGEX', '/([' . PONYDOCS_PRODUCTVERSION_LEGALCHARS . ']+)/' );
-define('PONYDOCS_PRODUCTVERSION_TITLE_REGEX', '/^' . PONYDOCS_DOCUMENTATION_PREFIX . '([' . PONYDOCS_PRODUCT_LEGALCHARS . ']+)' . PONYDOCS_PRODUCTVERSION_SUFFIX . '/' );
+pdefine('PONYDOCS_PRODUCTVERSION_SUFFIX', ':Versions' );
+pdefine('PONYDOCS_PRODUCTVERSION_LEGALCHARS', 'A-Za-z0-9_,.-' );
+pdefine('PONYDOCS_PRODUCTVERSION_REGEX', '/([' . PONYDOCS_PRODUCTVERSION_LEGALCHARS . ']+)/' );
+pdefine('PONYDOCS_PRODUCTVERSION_TITLE_REGEX', '/^' . PONYDOCS_DOCUMENTATION_PREFIX . '([' . PONYDOCS_PRODUCT_LEGALCHARS . ']+)' . PONYDOCS_PRODUCTVERSION_SUFFIX . '/' );
 
-define('PONYDOCS_PRODUCTMANUAL_SUFFIX', ':Manuals' );
-define('PONYDOCS_PRODUCTMANUAL_LEGALCHARS', 'A-Za-z0-9_,.-' );
-define('PONYDOCS_PRODUCTMANUAL_REGEX', '/([' . PONYDOCS_PRODUCTMANUAL_LEGALCHARS . ']+)/' );
-define('PONYDOCS_PRODUCTMANUAL_TITLE_REGEX', '/^' . PONYDOCS_DOCUMENTATION_PREFIX . '([' . PONYDOCS_PRODUCT_LEGALCHARS . ']+)' . PONYDOCS_PRODUCTMANUAL_SUFFIX . ':([a-zA-Z]{2})/' );
+pdefine('PONYDOCS_PRODUCTMANUAL_SUFFIX', ':Manuals' );
+pdefine('PONYDOCS_PRODUCTMANUAL_LEGALCHARS', 'A-Za-z0-9_,.-' );
+pdefine('PONYDOCS_PRODUCTMANUAL_REGEX', '/([' . PONYDOCS_PRODUCTMANUAL_LEGALCHARS . ']+)/' );
+pdefine('PONYDOCS_PRODUCTMANUAL_TITLE_REGEX', '/^' . PONYDOCS_DOCUMENTATION_PREFIX . '([' . PONYDOCS_PRODUCT_LEGALCHARS . ']+)' . PONYDOCS_PRODUCTMANUAL_SUFFIX . ':([a-zA-Z]{2})/' );
 
 // category cache expiration in seconds
-define('CATEGORY_CACHE_TTL', 300);
-define('PONYDOCS_TEMP_DIR', '/tmp/');
+pdefine('CATEGORY_CACHE_TTL', 300);
+pdefine('PONYDOCS_TEMP_DIR', '/tmp/');
 
 // Static Documentation
-define('PONYDOCS_STATIC_DIR', '/var/www/useruploads/docs/staticDocs');
-define('PONYDOCS_STATIC_PATH', 'DocumentationStatic');
-define('PONYDOCS_STATIC_URI', '/' . PONYDOCS_STATIC_PATH . '/');
+pdefine('PONYDOCS_STATIC_DIR', '/var/www/useruploads/docs/staticDocs');
+pdefine('PONYDOCS_STATIC_PATH', 'DocumentationStatic');
+pdefine('PONYDOCS_STATIC_URI', '/' . PONYDOCS_STATIC_PATH . '/');
 
 // specify URI to CSS file to dynamically override static documentation iframe CSS
-define('PONYDOCS_STATIC_CSS', '');
+pdefine('PONYDOCS_STATIC_CSS', '');
 
 // capitalization settings
-define('PONYDOCS_CASE_SENSITIVE_TITLES', false);
+pdefine('PONYDOCS_CASE_SENSITIVE_TITLES', false);
 
 // pdf output constants
 $uname = php_uname();
@@ -75,6 +94,5 @@ else if (preg_match('/x86_64/i', $uname))
 	$wkhtmlbin = 'wkhtmltopdf-amd64';
 else
 	$wkhtmlbin = 'wkhtmltopdf-i386';
-
-define('PONYDOCS_WKHTMLTOPDF_PATH', dirname(__FILE__) . "/bin/{$wkhtmlbin}");
+pdefine('PONYDOCS_WKHTMLTOPDF_PATH', dirname(__FILE__) . "/bin/{$wkhtmlbin}");
 
